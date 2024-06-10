56bb7cac14dae78a3e22f0b58fabc444df3cf0eb
Vnicy <208872015@qq.com>
2024-06-10 21:38:03 +0800

    首页功能

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+//请求的根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+//请求拦截器
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+//响应拦截器
+$http.afterRequest = function(){
+  uni.hideLoading()
+}
+
+//封装弹框的方法
+uni.$showMsg = function(title='数据请求失败！',duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
 
 // #ifndef VUE3
 import Vue from 'vue'
